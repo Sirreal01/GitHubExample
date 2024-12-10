2f7ed16474fabb1f28041912c754de03e899a6b9
Sirreal01 <sirreal01@comcast.net>
2024-12-10 09:36:01 -0500

    Initial Commit

---
+++ GitHubExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubExample
+//
+//  Created by ladon brown on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubExample/GitHubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExampleApp.swift
+//  GitHubExample
+//
+//  Created by ladon brown on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
